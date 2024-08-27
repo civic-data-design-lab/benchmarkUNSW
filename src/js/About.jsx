c3b6c01e160e9262b2d25951621beb5923718575
Karen Kuo <karenkuo0501@gmail.com>
2024-08-26 22:55:48 -0400

    updated about
    updated about

--- src/js/About.jsx
+++ src/js/About.jsx
@@ -4,9 +4,15 @@ function About() {
     return (
         <div>
             <h1>About Page</h1>
-            <section id="project">
+            <section id="project" className="nova-mono-regular">
                 <h2>Project</h2>
-                <p>Details about the project...</p>
+                <p>BenchMark is an experimental project that uses street furniture to measure the
+                    dynamics of public space and understand how UNSW students, particularly women,
+                    girls and gender diverse people, interact with and move seating in public space.</p>
+                <p>The seats include motion enabled lighting and were designed by female UNSW
+                    Industrial Design Students. Camera-vision sensors and onsite observation
+                    and surveys monitor and help us understand where and how they move.</p>
+                <p>This project has been funded by Transport for NSW through the Safer Cities program.</p>
             </section>
             <section id="data-analysis">
                 <h2>Data Analysis Result</h2>
--- src/js/Home.jsx
+++ src/js/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 
 function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the Home Page!</p>
-        </div>
+        <Container>
+            <Row>
+                <Col xs={6} md={4}>
+                    <Image src="../assets/react.svg" rounded />
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
