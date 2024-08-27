90ba73779ec79d3b233d11d86889bb91399b69f2
Karen Kuo <karen.kuo@corgan.com>
2024-08-26 22:33:57 -0400

    set up
    set up

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './style/App.css'
+
+import NavBar from './component/NavBar';
+import Home from './js/Home';
+import AiData from './js/AiData';
+import SurveyData from './js/SurveyData';
+import About from './js/About';
+import Downloads from './js/Downloads';
+
+function App() {
+    return (
+        <>
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/aidata" element={<AiData />} />
+                    <Route path="/surveydata" element={<SurveyData />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/downloads" element={<Downloads />} />
+                </Routes>
+            </Router>
+        </>
+    )
+}
+
+export default App
