c9f21dd3a0e93d3763fb0ed86b306292a33cb7f7
Minwook Kang <kmu973@naver.com>
2024-09-17 17:22:59 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/benchmarkUNSW/", // Uncomment and ensure this matches your repository name
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  build: {
+    outDir: "dist", // Ensure Vercel expects this directory
+  },
 });
