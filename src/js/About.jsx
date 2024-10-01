f127495518d0c8036b50079ba8c543b18b62e76d
Minwook Kang <kmu973@naver.com>
2024-10-01 10:36:42 -0400

    about photo colorized

--- src/js/About.jsx
+++ src/js/About.jsx
@@ -14,20 +14,20 @@ import reviewedInternallyIcon from "../assets/Symbols/Reviewed internally.svg";
 import cloudStorageIcon from "../assets/Symbols/CloudStorage.svg";
 
 import Carousel from "react-bootstrap/Carousel";
-import img1 from "../assets/About/1.jpg";
-import img2 from "../assets/About/2.jpg";
-import img3 from "../assets/About/3.jpg";
-import img4 from "../assets/About/4.jpg";
+import img1 from "../assets/About/colorized/1.jpg";
+import img2 from "../assets/About/colorized/2.jpg";
+import img3 from "../assets/About/colorized/3.jpg";
+import img4 from "../assets/About/colorized/4.jpg";
 
-import img5 from "../assets/About/a.jpg";
-import img6 from "../assets/About/b.jpg";
-import img7 from "../assets/About/c.jpg";
-import img8 from "../assets/About/d.jpg";
+import img5 from "../assets/About/colorized/a.jpg";
+import img6 from "../assets/About/colorized/b.jpg";
+import img7 from "../assets/About/colorized/c.jpg";
+import img8 from "../assets/About/colorized/d.jpg";
 
-import img9 from "../assets/About/5.jpg";
-import img10 from "../assets/About/6.jpg";
-import img11 from "../assets/About/7.jpg";
-import img12 from "../assets/About/8.jpg";
+import img9 from "../assets/About/colorized/5.jpg";
+import img10 from "../assets/About/colorized/6.jpg";
+import img11 from "../assets/About/colorized/7.jpg";
+import img12 from "../assets/About/colorized/8.jpg";
 
 function About() {
   const [openSections, setOpenSections] = useState({});
