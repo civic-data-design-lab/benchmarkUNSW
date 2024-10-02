3d3e9fe6622a585c67a163e2c4ad0386a46dbbca
Karen Kuo <karenkuo0501@gmail.com>
2024-10-01 21:19:17 -0400

    fix build error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./style/Main.css";
 import "./style/App.css";
-import "./style/Navbar.css";
+import "./style/NavBar.css";
 import "./style/Font.css";
 
 createRoot(document.getElementById("root")).render(
