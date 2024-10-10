8d074f8e71ea0f727c2a3af2e5862ba4d3e8609a
Minwook Kang <kmu973@naver.com>
2024-10-10 11:30:06 -0400

    google analytics setting changes using reactGA

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import "./style/App.css";
 import NavBar from "./component/Navbar";
 import Footer from "./component/Footer";
@@ -8,32 +12,48 @@ import ExploreData from "./js/ExploreData";
 import About from "./js/About";
 import Findings from "./js/Findings";
 
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
+
+const TRACKING_ID = "G-XWRNLMFGEM"; // Replace with your Google Analytics ID
+ReactGA.initialize(TRACKING_ID);
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Send page view to Google Analytics when the location changes
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
-    <>
-      <Router>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            // backgroundColor: "#FFEFF3",
-            backgroundColor: "#FF2551",
-          }}
-        >
-          <div className="app-container">
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/exploredata" element={<ExploreData />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/findings" element={<Findings />} />
-            </Routes>
-            <Footer />
-          </div>
-        </div>
-      </Router>
-    </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: "#FF2551", // Or your preferred color
+      }}
+    >
+      <div className="app-container">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exploredata" element={<ExploreData />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/findings" element={<Findings />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default AppWrapper;
