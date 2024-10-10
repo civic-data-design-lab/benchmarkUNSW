eeeb797f2a06a77273dcd41b4f7fd48f5d38f94d
Minwook Kang <kmu973@naver.com>
2024-10-10 13:38:05 -0400

    add page tracker

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,24 @@ import ReactGA from "react-ga4";
 
 const TRACKING_ID = "G-XWRNLMFGEM"; // Replace with your Google Analytics ID
 ReactGA.initialize(TRACKING_ID);
+ReactGA.set({ debug: true });
 
 function App() {
   const location = useLocation();
 
   useEffect(() => {
+    // Set the page title to the current location path
+    const pageTitle = location.pathname.replace("/", "") || "Home";
+    document.title = pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
+
+    // console.log("page is ", location.pathname);
+    // console.log("title is ", document.title);
     // Send page view to Google Analytics when the location changes
-    ReactGA.send({ hitType: "pageview", page: location.pathname });
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname,
+      title: document.title,
+    });
   }, [location]);
 
   return (
